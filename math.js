2cb285304c94dc2708b204ef8c86da8b5f82eaa8
Berlyn Araniva <araniva_berlyn_ale01@student.smc.edu>
2026-07-06 21:31:24 -0700

    Add isOdd function

--- math.js
+++ math.js
@@ -10,6 +10,11 @@ function isEven(n) {
   return n % 2 === 0;
 }
 
+function isOdd(n) {
+  return n % 2 !== 0;
+}
+
 console.log(double(5));
 console.log(square(4));
 console.log(isEven(6));
+console.log(isOdd(7));
